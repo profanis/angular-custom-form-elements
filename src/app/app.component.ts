b3ffc50ddd4712a4e3f493a931d03ba9c84c5834
profanis <fanis@projectbeagle.com>
2020-01-22 15:10:16 +0200

    form with no abstractions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'custom-form-elements';
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  private buildForm() {
+this.myForm = this.fb.group({
+  firstName: [null, [Validators.required, Validators.minLength(10)]],
+  lastName: [null, [Validators.required, Validators.minLength(10)]],
+  email: [null, [Validators.required, Validators.email]],
+  password: [null, Validators.required],
+});
+  }
 }
